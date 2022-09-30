bc9c343bc542e4ff92e92b84cbd8b23d953c9ef6
coderMichaelLapshyn2345 <114723749+coderMichaelLapshyn2345@users.noreply.github.com>
2022-09-30 18:10:22 +0200

    Add files via upload

---
+++ InputTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Input_Ouput._03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            try
+            {
+                Console.WriteLine("Введіть число: ");
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ви ввели число " + number);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Помилка введення! Введіть число!");
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
